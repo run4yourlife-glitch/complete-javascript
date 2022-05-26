7d5f3ef7aa918f3ac54e885926d21f111fece918
run4yourlife-glitch <jomariabejo@gmail.com>
2022-05-26 21:07:24 +0800

    update

--- 11-Arrays-Bankist/starter/codingchallenge#1.js
+++ 11-Arrays-Bankist/starter/codingchallenge#1.js
@@ -20,28 +20,44 @@ TEST DATA 2: Julia's data [9, 16, 6, 8, 3], Kate's data [10, 5, 6, 1, 4]
 GOOD LUCK 😀
 */
 
-const Julia = [3, 5, 2, 12, 7]
-Julia.shift();
-Julia.pop();
-const Kate =  [4, 1, 15, 8, 3]
+// const Julia = [3, 5, 2, 12, 7]
+// Julia.shift();
+// Julia.pop();
+// const Kate =  [4, 1, 15, 8, 3]
+// const Jomari =  [25,2,5,17,1]
+// const Kate_Jom = Kate.concat(Jomari)
+// const Julia_Final = Kate_Jom.concat(Julia)
 
-function checkDogs(arr){
-    let mySet1 = new Set(arr)
-    let num = 0;
-    console.log(mySet1)
-    mySet1.forEach(arr => {
-        let result = arr>3 ? "Adult" : "Puppy";
-        num++;
-        console.log(`Dog number ${num} is an ${result} `)
+// function checkDogs(arr){
+//     let mySet1 = new Set(arr)
+//     let num = 0;
+//     console.log(mySet1)
+//     mySet1.forEach(arr => {
+//         let result = arr>3 ? "Adult" : "Puppy";
+//         num++;
+//         console.log(`Dog number ${num} is an ${result} `)
+//     });
+// }
+// checkDogs(Julia_Final)
+// console.log(Julia_Final.length)
+const checkDogs = function (dogsJulia, dogsKate) {
+    const dogsJuliaCorrected = dogsJulia.slice();
+    dogsJuliaCorrected.splice(0, 1);
+    dogsJuliaCorrected.splice(-2);
+    // dogsJulia.slice(1, 3);
+    const dogs = dogsJuliaCorrected.concat(dogsKate);
+    console.log(dogs);
+  
+    dogs.forEach(function (dog, i) {
+      if (dog >= 3) {
+        console.log(`Dog number ${i + 1} is an adult, and is ${dog} years old`);
+      } else {
+        console.log(`Dog number ${i + 1} is still a puppy 🐶`);
+      }
     });
-}
-checkDogs(Julia.concat(Kate))
-
-
-
-
-
-
+  };
+  // checkDogs([3, 5, 2, 12, 7], [4, 1, 15, 8, 3]);
+  checkDogs([9, 16, 6, 8, 3], [10, 5, 6, 1, 4]);
 
 
 
