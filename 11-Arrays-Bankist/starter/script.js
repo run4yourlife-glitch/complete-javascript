0386d7ecb2b26ca9bab84cd3acc186cf3aa41939
run4yourlife-glitch <jomariabejo@gmail.com>
2022-05-26 15:37:24 +0800

    working with array coding challenge number 1

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -70,14 +70,19 @@ const displayMovements = function (movements) {
 
     const html = `
       <div class="movements__row">
-        <div class="movements__type movements__type--${type}">${i + 1} ${type}</div>
-        <div class="movements__value">${mov}</div>
+        <div class="movements__type movements__type--${type}">
+          ${i + 1} ${type}
+        </div>
+        <div class="movements__value">
+          ${mov}
+        </div>
       </div>
     `;
     containerMovements.insertAdjacentHTML('afterbegin', html);
   });
 };
 displayMovements(account1.movements)
+console.log(containerMovements.innerHTML)
 
 
 
